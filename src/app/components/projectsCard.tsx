6dee032047d720147b4e8cf579e5efe4cbb1a974
nomanbrohi <nomanbrohi9@gmail.com>
2024-10-27 05:03:12 +0500

    Image issue has been resolved

--- src/app/components/projectsCard.tsx
+++ src/app/components/projectsCard.tsx
@@ -12,7 +12,7 @@ const ProjectCard = (props: ProjectCardProps)  => {
   return (
     <div className="bg-white shadow-md rounded-lg overflow-hidden max-w-sm mx-auto transition-transform duration-300 ease-out hover:scale-105 hover:shadow-2xl cursor-pointer">
       {/* Project Image */}
-      <Image src={imageSrc} alt={title} width={0} height={0} className="w-full h-48 object-cover" />
+      <Image src={imageSrc} alt={title} width={300} height={300} className="w-full h-48 object-cover" />
       
       {/* Card Content */}
       <div className="p-4 text-center">
